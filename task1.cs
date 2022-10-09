5577cee7fe4bbf2fd723d3ff9c02a4000bb82d64
Саврацький Євгеній <33554364+Jekon4ik@users.noreply.github.com>
2022-10-09 22:01:09 +0300

    task1

--- task1.cs
+++ task1.cs
@@ -1,10 +1,70 @@
-﻿Console.Write("Перша сторона трикутника: ");
-double a = Convert.ToDouble(Console.ReadLine());
-Console.Write("Друга сторона трикутника: ");
-double b = Convert.ToDouble(Console.ReadLine());
-Console.Write("Третя сторона трикутника: ");
-double c = Convert.ToDouble(Console.ReadLine());
-double p = (a + b + c) / 2;
-Console.WriteLine("Площа трикутника: " + Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
-
-Console.ReadKey();
+// 3найти мінімум з 4 цілочисельних чисел
+using System; 
+class Task1 {
+        static void Main(string[] arg)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            Console.Write("Перше число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Друге число: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Третє число: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Четверте число: ");
+            d = Convert.ToInt32(Console.ReadLine());
+            if (a < b)
+            {
+                if (a < c)
+                {
+                    if (a < d)
+                    {
+                        Console.WriteLine("Найменше число: " + a);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Найменше число: " + d);
+                    }
+                }
+                else
+                {
+                    if (c < d)
+                    {
+                        Console.WriteLine("Найменше число: " + c);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Найменше число: " + d);
+                    }
+                }
+            }
+            else
+            {
+                if (b < c)
+                {
+                    if (b < d)
+                    {
+                        Console.WriteLine("Найменше число: " + b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Найменше число: " + d);
+                    }
+                }
+                else
+                {
+                    if (c < d)
+                    {
+                        Console.WriteLine("Найменше число: " + c);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Найменше число: " + d);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
