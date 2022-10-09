e9647a5cac0e63126146c1a15996d955df195c57
Саврацький Євгеній <33554364+Jekon4ik@users.noreply.github.com>
2022-10-09 21:54:55 +0300

    task1
    1) знайти мінімум з 4 цілочисельних чисел

---
+++ task1.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Перша сторона трикутника: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Друга сторона трикутника: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Третя сторона трикутника: ");
+double c = Convert.ToDouble(Console.ReadLine());
+double p = (a + b + c) / 2;
+Console.WriteLine("Площа трикутника: " + Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
+
+Console.ReadKey();
